07a32d8e94bceb21347e8caf41662ec04c1ecdf5
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-11 20:44:49 +0600

    update seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,63 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+// seo
 export const metadata: Metadata = {
-  title: "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয় | Imampur Palli Mangal High School",
-  description: "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয় officeal website",
+  title: "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয় | IPMHS",
+  description:
+    "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়, কালিপুর বাজার, মতলব উত্তর, চাঁদপুর। অফিসিয়াল ওয়েবসাইটে স্কুল সম্পর্কিত তথ্য, নোটিশ, রেজাল্ট এবং অন্যান্য গুরুত্বপূর্ণ তথ্য পাওয়া যাবে।",
+  keywords: [
+    "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়",
+    "Imampur Palli Mangal High School",
+    "Officeal website of Imampur Palli Mangal High School",
+    "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয় ওয়েবসাইট ",
+    "অফিসিয়াল ওয়েবসাইট - ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়",
+    "IPMHS",
+    "কালিপুর বাজার স্কুল",
+    "মতলব উত্তর স্কুল",
+    "চাঁদপুর স্কুল",
+    "চাঁদপুরের শ্রেষ্ঠ স্কুল",
+    "বাংলাদেশ উচ্চ বিদ্যালয়",
+    "স্কুল ভর্তি তথ্য",
+    "স্কুল ফলাফল",
+  ],
+  authors: [{ name: "Imampur Palli Mangal High School" }],
+  viewport: "width=device-width, initial-scale=1",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    title: "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়",
+    description:
+      "অফিসিয়াল ওয়েবসাইট - Imampur Palli Mangal High School, কালিপুর বাজার, চাঁদপুর",
+    url: "https://ipmhs.vercel.app",
+    siteName: "IPMHS",
+    images: [
+      {
+        url: "https://ipmhs.vercel.app/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়",
+      },
+    ],
+    locale: "bn_BD",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়",
+    description:
+      "অফিসিয়াল ওয়েবসাইট - Imampur Palli Mangal High School, কালিপুর বাজার, চাঁদপুর",
+    images: ["https://ipmhs.vercel.app/images/og-image.png"],
+  },
 };
 
 export default function RootLayout({
